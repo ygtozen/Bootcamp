b46598011657a85cb674ae87fb693c276269ed33
ygtozen <ozenyigit151@gmail.com>
2022-05-31 12:37:32 +0300

    31 mayıs calismasi

---
+++ 31-05-22/js/templates.js
@@ -0,0 +1,135 @@
+// JS --> tek satırlık yorumlama
+/*çok satırlı yorumlama (multi-line comment) */
+
+//console.log("Merhaba")
+// pop-up alert
+/* alert("deneme")
+window.alert("deneme window") */
+
+/////////////////////////////////////
+// window >> Hepsi
+// document >> HTML
+// alert("pop-up")
+//alert("windows pop-up")
+// console.log("asd")
+
+// document.write("deneme")
+/////////////////////////////////////
+
+/* Kullanıcan Veri Almak
+var name = prompt("Adınız");
+console.log(name) 
+*/
+
+/////////////////////////////////////
+// Variable
+// var isim = "Yiğit";
+// isim = "Yiğit Ö"; // String
+// isim = true; // boolen
+// isim = 44; // number - int
+// console.log(isim);
+
+// var soyisim = "Özen";
+// $soyisim = "Özen"
+// console.log(soyisim);
+
+// camselCase ile değişken adlarını oluşturalım
+// unique
+// İngilizce kelimler
+
+/////////////////////////////////////
+// JS " ", '' , ` `, " ' ' " 
+
+// variable initialization
+// var deneme;  undefined - tanımsız
+// console.log(deneme)
+
+/////////////////////////////////////
+// Operator
+// logic operator         :  && = VE || = VEYA
+// mathematics operator   :  + - * / %
+// comparations(karşılaştırma) operators : > >= < <= ! == 
+// = atama , == eşittir, === eşitmi (türüne bak)
+// postfix --> x++, ++x, y--, --y
+
+// bit < byte < GB < TB
+// 1 byte = 8 bit
+// 1 bit = 0
+// Binary Codes
+// Octal (8) --> (01234567)
+// decimal (10) --> (0123456789)
+// hexadecmal (16) --> (0123456789abcdef)
+
+/////////////////////////////////////
+// var result = "Malatya";
+// console.log(typeof(result)) // typeof --> tür bulmak içn
+
+// var result2 = 44;
+// console.log(typeof(result2))
+
+// var result = "Malatya";
+// console.log(typeof(result))
+
+// var result3 = 4>1;
+// console.log(typeof result3 )
+
+////////////////////////////////////
+// Örnek-1
+// var x1, x2, x3;
+// x1 = 9;
+// x2 = 2;
+// x3 = x1 + x2;
+// console.log("Toplam : " + x3)
+
+// x3 = x1 * x2;
+// console.log(x3)
+
+// x3 = x1 / x2;
+// console.log(x3)
+
+// x3 = x1 % x2;
+// console.log(x3)
+
+////////////////////////////////////
+
+// CAST : dönüştürmek
+// string number boolen 
+// database - api - socket : Database'den veya apiden gelen değerler string'dir fakat biz bunları int dönüştürmek isteyebiliriz.
+
+// Number()
+// String()
+// var x1 = Number(prompt("Lütfen bir sayı giriniz :"));
+// console.log(x1 * x1);
+
+////////////////////////////////////
+
+// undefined : tanımlı değer vermezsek bu sorunu yaşarız
+// var x1;
+// console.log(x1)
+
+// NaN : sayı değil
+// var x2 = 9/"asd";
+// console.log(x2);
+
+// İsNaN
+
+
+// Infinity : sonsuz
+// var3 = 8 / 0;
+// console.log(var3);
+
+// new : ram hafızasında yer açmak (Doğar)
+
+// null : hafızayı boşaltmak - boş değer döndüğünde
+// x4 = null;
+
+// sıfır : bir değeri vardır, çarpmada yutan , toplamada etkisiz elemandır
+
+////////////////////////////////////
+// escape karakter
+console.log("\"");
+console.log("'");
+
+
+////////////////////////////////////
+// Math
